14720c69d640cf3f9f6958890ac90d41f28b7dfb
Amrit Bhogal <ambhogal01@gmail.com>
2022-06-26 22:09:38 -0700

    Unity time

--- Client/src/include/common.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define super(T) $super_##T##$
-#define inherit(T) union { struct T; struct T super(T); }
-
-#define pure __attribute__((pure))
-#define constructor __attribute__((constructor))
-#define destructor __attribute__((destructor))
--- xmake.lua
+++
@@ -1,8 +0,0 @@
-includes("Client")
-
-target("WurstLink")
-do
-    set_kind("phony")
-    add_deps("WurstLink-Client")
-end
-target_end()
